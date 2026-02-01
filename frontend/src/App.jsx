08c917667c0c8b454531ce018792983ff48a14e7
sakshipriya683-beep <sakshipriya683@gmail.com>
2026-02-01 17:57:27 +0530

    Task 1 completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CreatePaper from "./create-paper.jsx";
+import CreatePaper from "./components/create_paper";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/create-paper" element={<CreatePaper />} />
+        {/* HOME ROUTE */}
+        <Route path="/" element={<CreatePaper />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
